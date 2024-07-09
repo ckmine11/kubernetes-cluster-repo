b3f649e4f013d8937a300f7d68ab7104be1feeb1
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-09 21:40:32 +0530

    Update kuberinstallation.sh

--- kuberinstallation.sh
+++ kuberinstallation.sh
@@ -25,15 +25,14 @@ systemctl start crio
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.25/rpm/
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.25/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
-dnf install -y kubelet-1.24.0 -y kubeadm-1.24.0  -y kubectl-1.24.0 --disableexcludes=kubernetes
+dnf install -y kubelet-1.25.0 -y kubeadm-1.25.0  -y kubectl-1.25.0 --disableexcludes=kubernetes
 
 systemctl enable kubelet
 systemctl start kubelet
